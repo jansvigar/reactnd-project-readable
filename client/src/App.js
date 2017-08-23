6779fcef7acd015871c785720fc5a3b17c0a3e76
Jan Vigar <jansvigar@gmail.com>
2017-08-22 22:21:15 -0700

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () =>
+  (<div className="App">
+    <h1>Readable</h1>
+    <div className="posts">
+      <p>test post</p>
+    </div>
+  </div>);
+
+export default App;
